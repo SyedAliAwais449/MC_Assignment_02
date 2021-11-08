903d362c924d193d34d94257de004377d461b4ff
SyedAliAwais449 <saliawaisd449@gmail.com>
2021-11-08 17:49:08 +0500

    Main activity added
    Main activity having name and student id with some button

--- app/src/main/java/com/example/assignment_02/MainActivity.java
+++ app/src/main/java/com/example/assignment_02/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.assignment_02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.button:
+                String URL= "https://github.com/SyedAliAwais449/MC_Assignment_02";
+                Uri webpage = Uri.parse(URL);
+                intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+                break;
+        }
+    }
+
 }
